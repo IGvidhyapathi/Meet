42a081bb401646d73c189df46a8a7da8af3b42f2
IGvidhyapathi <c.vidhyapathi1555@gmail.com>
2024-08-14 22:41:44 +0530

    clerk ui update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import { dark, neobrutalism, shadesOfPurple} from '@clerk/themes'
 
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
@@ -25,17 +26,12 @@ export default function RootLayout({
     <html lang="en">
       <ClerkProvider
         appearance={{
+          baseTheme:[dark],
           layout: {
             socialButtonsVariant: "iconButton",
             logoImageUrl: "/icons/logo.svg",
           },
-          variables: {
-            colorText: "#fff",
-            colorPrimary: "#0E78F9",
-            colorBackground: "#154c79",
-            colorInputBackground: "#252A41",
-            colorInputText: "#fff",
-          },
+          
         }}
       >
         <body className={`${inter.className} bg-dark-2`}>
