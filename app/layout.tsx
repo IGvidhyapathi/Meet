afcd7bce731e38376f5f8cda22d6dbdaf64b2df5
IGvidhyapathi <c.vidhyapathi1555@gmail.com>
2024-08-14 16:35:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,12 +27,12 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+            logoImageUrl: "/icons/logo.svg",
           },
           variables: {
             colorText: "#fff",
             colorPrimary: "#0E78F9",
-            colorBackground: "#1C1F2E",
+            colorBackground: "#154c79",
             colorInputBackground: "#252A41",
             colorInputText: "#fff",
           },
